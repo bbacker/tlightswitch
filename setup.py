e38f8014b57f484c5c5bff8fbb8130ade532d109
Bryan Backer <bbacker@ep.com>
2018-03-02 11:59:34 -0800

    adjusting layout to feed pip install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='taglightswitch',
+        version='0.1',
+        description='turn EC2 instances off based on tags',
+        url='http://github.com/bbacker/taglightswitch',
+        author='Bryan Backer',
+        author_email='bryan.backer@gmail.com',
+        license='MIT',
+        packages=['taglightswitch'],
+        zip_safe=False)
+
