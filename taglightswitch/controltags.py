5adb5f6f87d332c5fcf41491630263926327f032
Bryan Backer <bbacker@ep.com>
2018-03-02 15:36:12 -0800

    range work

--- taglightswitch/controltags.py
+++ taglightswitch/controltags.py
@@ -15,9 +15,10 @@ class ControlTags:
     def get_target_tag_name(cls):
         return 'lightswitch:timerange'
 
-    # take AWS tag body as CSV string, parse into dict
     @classmethod
-    def parse_csvbody(cls,bodystr):
+    def _parse_csvbody(cls,bodystr):
+        """ take AWS tag body as CSV string, parse into dict.
+        e.g. start=19:00,end=07:00"""
         body = {}
         items = bodystr.split(',')
         rx = re.compile(r'\s*(?P<key>\w+)\s*=\s*(?P<value>.*)')
@@ -36,14 +37,20 @@ class ControlTags:
         return t.time()
 
     @classmethod
-    def _parse_timerange(cls,body):
-        """ take AWS tag body, parse into start, end times""
+    def parse_timerange(cls,body):
+        """ take AWS tag body, parse into start, end times"""
         range_dict = cls._parse_csvbody(body)
         s = cls._parse_time(range_dict['start'])
         e = cls._parse_time(range_dict['end'])
         return s, e
 
     @classmethod
-    def time_is_within_range(cls, range, time):
+    def time_is_within_range(cls, start, end, time):
         """ does the given time fall within the timerange? """
-        return True
+
+        if start < end:
+            return time >= start and time <= end
+
+        # if end time smaller than start, assume the clock has 'wrapped',
+        # e.g. 10pm to 7am, so check time is after s or before e
+        return time >= end or time <= start
--- taglightswitch/tests/test_tag_manipulation.py
+++ taglightswitch/tests/test_tag_manipulation.py
@@ -6,7 +6,7 @@ from pprint import pprint
 cls = controltags.ControlTags
 
 def range_helper(rangestr):
-    r = cls._parse_timerange(rangestr)
+    r = cls.parse_timerange(rangestr)
     assert r
     assert r.start
     assert r.end
@@ -24,4 +24,22 @@ def test_csv():
 def test_ranges():
     tenPM = datetime.time(10 + 12, 0)
     eightAM = datetime.time(8,0)
-    assert (tenPM,eightAM) == cls._parse_timerange("start=22:00,end=08:00")
+    assert (tenPM,eightAM) == cls.parse_timerange("start=22:00,end=08:00")
+
+def test_within_range():
+    elevenPM = datetime.time(11 + 12, 0)
+    fivePM = datetime.time(5 + 12, 0)
+    tenPM = datetime.time(10 + 12, 0)
+    eightAM = datetime.time(8,0)
+    sixAM = datetime.time(6,0)
+    # daytime off
+    assert cls.time_is_within_range(eightAM, tenPM, fivePM)
+    assert not cls.time_is_within_range(eightAM, fivePM, tenPM)
+
+    # nighttime off
+    assert cls.time_is_within_range(tenPM, eightAM, tenPM)
+    assert cls.time_is_within_range(tenPM, eightAM, sixAM)
+    assert cls.time_is_within_range(tenPM, eightAM, fivePM)
+
+    # corner case
+    assert cls.time_is_within_range(eightAM, eightAM, eightAM)
