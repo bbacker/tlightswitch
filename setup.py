3924326c6f9f368863495cc711072c6306ff7c1e
Bryan Backer <bbacker@ep.com>
2018-03-21 13:01:44 -0700

    fix long desc

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from setuptools import setup
 setup(name='taglightswitch',
         version='0.1.1',
         description='schedule EC2 instances on/off based on tags',
-        long_description="""power EC2 instances on and off on a schedule based
-        on AWS tags applied to the instances. see
-        https://github.com/bbacker/taglightswitch for more information""",
+        long_description='Tag based EC2 instance scheduler. See https://github.com/bbacker/taglightswitch for more information',
         url='http://github.com/bbacker/taglightswitch',
         author='Bryan Backer',
         author_email='bryan.backer@gmail.com',
