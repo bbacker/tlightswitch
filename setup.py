a7f35ebbe3cd5689ea601314bb6941ff378a27cd
Bryan Backer <bbacker@ep.com>
2018-03-22 14:50:18 -0700

    list prereq pkgs

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='taglightswitch',
-        version='0.1.1',
+        version='0.1.2',
         description='schedule EC2 instances on/off based on tags',
         long_description='Tag based EC2 instance scheduler. See https://github.com/bbacker/taglightswitch for more information',
         url='http://github.com/bbacker/taglightswitch',
@@ -10,5 +10,7 @@ setup(name='taglightswitch',
         license='MIT',
         packages=['taglightswitch'],
         scripts=['taglightswitch/check_lightswitches'],
-        zip_safe=False)
-
+        zip_safe=False,
+        install_requires=[
+            'boto3',
+        ])
